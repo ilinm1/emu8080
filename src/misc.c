e309dc8302e42338a297a899a488ef5574144dfc
IlinMihail <57045293+ilinm1@users.noreply.github.com>
2025-02-15 23:03:16 +0100

    Update misc.c

--- src/misc.c
+++ src/misc.c
@@ -2,28 +2,9 @@
 #include <windows.h>
 #endif
 
-#ifdef __linux__
-#include <unistd.h>
-#include <time.h>
-#include <errno.h>
-#endif
-
 void SleepMilli(int ms)
 {
     #ifdef _WIN32
     Sleep(ms);
-    #endif
-
-    #ifdef __linux__
-    //https://stackoverflow.com/questions/1157209/is-there-an-alternative-sleep-function-in-c-to-milliseconds
-    struct timespec ts;
-    int res;
-
-    ts.tv_sec = ms / 1000;
-    ts.tv_nsec = (ms % 1000) * 1000000;
-
-    do {
-        res = nanosleep(&ts, &ts);
-    } while (res && errno == EINTR);
     #endif
 }
